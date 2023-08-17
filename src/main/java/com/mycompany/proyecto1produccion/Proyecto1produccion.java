6d8c853fa3754c680f9c9ce83527d94ac4be6948
ArminMG <lamgxela@gmail.com>
2023-08-17 13:04:45 -0600

    primer commit

---
+++ src/main/java/com/mycompany/proyecto1produccion/Proyecto1produccion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1produccion;
+
+/**
+ *
+ * @author lamg_
+ */
+public class Proyecto1produccion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
